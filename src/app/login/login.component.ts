300bb99b71f3c43f005f3d150059cb1afa2e85e3
Abdessamad ABOUKDIR <a.abdess16@gmail.com>
2017-12-29 17:39:56 +0000

    prevent removing manually btn disabled attribute

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {catchError, tap} from "rxjs/operators";
 import {Router} from "@angular/router";
@@ -11,39 +11,46 @@ import {AppService} from "../app.service";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  email;
-  password;
+  email: string;
+  password: string;
   failedAuthentication;
+  emptyField = false;
 
-  constructor(private http: HttpClient, private router: Router, private _appService: AppService) { }
+  constructor(private http: HttpClient, private router: Router, private _appService: AppService) {
+  }
 
   ngOnInit() {
-    if(localStorage.getItem('currentUser')) {
+    if (localStorage.getItem('currentUser')) {
       this.router.navigate(["/nearbyShops"]);
     }
 
     this.failedAuthentication = false;
   }
 
-  login(): void{
-    this.http.get<User>(`http://localhost:8080/authentication?email=${this.email}&password=${this.password}`)
-      .subscribe(user => {
-        if(user){
-          console.log(user);
+  login(): void {
+    if(this.email.trim().length === 0 || this.password.trim().length === 0){
+      this.emptyField = true;
+    }
+    else{
+      this.http.get<User>(`http://localhost:8080/authentication?email=${this.email}&password=${this.password}`)
+        .subscribe(user => {
+            if (user) {
+              console.log(user);
 
-          let curentUser = {
-            username: user.username,
-            token: btoa(`${user.email}:${user.password}`)
-          };
-          localStorage.setItem('currentUser', JSON.stringify(curentUser));
-          this.router.navigate(["/nearbyShops"]);
-          // reload navbar
-          location.reload();
-        }else{
-          this.failedAuthentication = true;
-        }
-      },
-        error => alert('Error:\n'+error.name));
+              let curentUser = {
+                username: user.username,
+                token: btoa(`${user.email}:${user.password}`)
+              };
+              localStorage.setItem('currentUser', JSON.stringify(curentUser));
+              this.router.navigate(["/nearbyShops"]);
+              // reload navbar
+              location.reload();
+            } else {
+              this.failedAuthentication = true;
+            }
+          },
+          error => alert('Error:\n' + error.name));
+    }
   }
 
 }
